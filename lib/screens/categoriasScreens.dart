f03106180fc2fbc14fdd9f3327d553709bfd6f3b
Anfacibry <49923143+Anfacibry@users.noreply.github.com>
2021-05-17 15:16:39 -0300

    adicionando screens

---
+++ lib/screens/categoriasScreens.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CategoriaScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GridView(
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 200,
+        childAspectRatio: 2 / 1,
+        crossAxisSpacing: 20,
+        mainAxisSpacing: 20,
+      ),
+    );
+  }
+}
