3d525c89d190fc24b368a859e03f002eaf0bebde
Anfacibry <49923143+Anfacibry@users.noreply.github.com>
2021-05-17 14:50:27 -0300

    first commit

---
+++ lib/data/dummy_data.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:navegacao/models/categoria.dart';
+
+List<Categoria> dummyCatgorias = [
+  Categoria(
+    id: "c1",
+    title: "Italiano",
+    color: Colors.purple,
+  ),
+  Categoria(
+    id: 'c2',
+    title: 'Rápido & Fácil',
+    color: Colors.red,
+  ),
+  Categoria(
+    id: 'c3',
+    title: 'Hamburgers',
+    color: Colors.orange,
+  ),
+  Categoria(
+    id: 'c4',
+    title: 'Alemã',
+    color: Colors.amber,
+  ),
+  Categoria(
+    id: 'c5',
+    title: 'Leve & Saudável',
+    color: Colors.amber,
+  ),
+  Categoria(
+    id: 'c6',
+    title: 'Exótica',
+    color: Colors.green,
+  ),
+  Categoria(
+    id: 'c7',
+    title: 'Café da Manhã',
+    color: Colors.lightBlue,
+  ),
+  Categoria(
+    id: 'c8',
+    title: 'Asiática',
+    color: Colors.lightGreen,
+  ),
+  Categoria(
+    id: 'c9',
+    title: 'Francesa',
+    color: Colors.pink,
+  ),
+  Categoria(
+    id: 'c10',
+    title: 'Verão',
+    color: Colors.teal,
+  ),
+];
