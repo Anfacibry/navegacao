8e8c042ae74f24356f95e06a56abc8c56970fcae
Anfacibry <49923143+Anfacibry@users.noreply.github.com>
2021-06-01 15:34:10 -0300

    atualizando screens e pubspec.lock

--- lib/screens/categoriasScreens.dart
+++ lib/screens/categoriasScreens.dart
@@ -5,16 +5,22 @@ import 'package:navegacao/data/dummy_data.dart';
 class CategoriaScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
-        childAspectRatio: 2 / 1,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("Vamos conzinhar!"),
       ),
-      children: dummyCatgorias.map((cat) {
-        return CategoriaItem(cat);
-      }).toList(),
+      body: GridView(
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 2 / 1,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: dummyCatgorias.map((cat) {
+          return CategoriaItem(cat);
+        }).toList(),
+      ),
     );
   }
 }
