0629bf1c363488ce739cdc3526c7455f490573c9
Anfacibry <49923143+Anfacibry@users.noreply.github.com>
2021-06-03 15:42:25 -0300

    atualiando componentes

--- lib/components/categoria_item.dart
+++ lib/components/categoria_item.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:navegacao/models/categoria.dart';
+import 'package:navegacao/screens/categoriasMailsScreens.dart';
 
 class CategoriaItem extends StatelessWidget {
   final Categoria categoria;
@@ -8,20 +9,34 @@ class CategoriaItem extends StatelessWidget {
     Key? key,
   }) : super(key: key);
 
+  void _navegandoTelas(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (_) {
+          return CategoriaMailsScreens();
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(10),
-      child: Text(categoria.title!),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(20),
-        gradient: LinearGradient(
-          colors: [
-            categoria.color!.withOpacity(0.4),
-            categoria.color!,
-          ],
-          begin: Alignment.topRight, //do topo esquerdo
-          end: Alignment.bottomLeft, //para o baixo direito
+    return InkWell(
+      onTap: () => _navegandoTelas(context),
+      borderRadius: BorderRadius.circular(20),
+      child: Container(
+        padding: const EdgeInsets.all(10),
+        child: Text(categoria.title!),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          gradient: LinearGradient(
+            colors: [
+              categoria.color!.withOpacity(0.4),
+              categoria.color!,
+            ],
+            begin: Alignment.topRight, //do topo esquerdo
+            end: Alignment.bottomLeft, //para o baixo direito
+          ),
         ),
       ),
     );
