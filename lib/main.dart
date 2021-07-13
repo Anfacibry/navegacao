a606f753c515ee4f2c4d790992ccfe20dd32087b
Anfacibry <anfacibry@gmail.com>
2021-07-13 15:16:38 -0300

    add tudo

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:navegacao/screens/categoriasScreens.dart';
+import 'package:navegacao/utils/rotas_nomeadas.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,27 +10,29 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'DeliMeals',
-      theme: ThemeData(
-        primarySwatch: Colors.pink,
-        accentColor: Colors.amber,
-        fontFamily: 'Raleway',
-        canvasColor: Color.fromRGBO(255, 254, 229, 1),
-        textTheme: TextTheme(
-            headline6: TextStyle(
-              fontSize: 20,
-              fontFamily: 'RobotoCondensed',
-              fontWeight: FontWeight.bold,
-              color: Colors.white,
-            ),
-            bodyText2: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-            )),
-      ),
-      home: CategoriaScreen(),
-    );
+        debugShowCheckedModeBanner: false,
+        title: 'DeliMeals',
+        theme: ThemeData(
+          primarySwatch: Colors.pink,
+          accentColor: Colors.amber,
+          fontFamily: 'Raleway',
+          canvasColor: Color.fromRGBO(255, 254, 229, 1),
+          textTheme: TextTheme(
+              headline6: TextStyle(
+                fontSize: 20,
+                fontFamily: 'RobotoCondensed',
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+              bodyText2: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              )),
+        ),
+        initialRoute: AppRotas.HOME,
+        routes: {
+          AppRotas.HOME: (ctx) => CategoriaScreen(),
+        });
   }
 }
 
---
+++ lib/utils/rotas_nomeadas.dart
@@ -0,0 +1,4 @@
+class AppRotas {
+  static const HOME = "/";
+  static const PAGE_CATEGORIA = "/pagecategoria";
+}
