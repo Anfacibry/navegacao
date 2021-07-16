45fab44244e96510feed25cee50377c31be77121
Anfacibry <anfacibry@gmail.com>
2021-07-16 15:35:54 -0300

    lib

--- lib/screens/categoriasMailsScreens.dart
+++ lib/screens/categoriasMailsScreens.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:navegacao/data/dummy_data.dart';
 import 'package:navegacao/models/categoria.dart';
+import 'package:navegacao/models/mealItem.dart';
 
 class CategoriaMailsScreens extends StatelessWidget {
   final Categoria categoria;
@@ -7,12 +9,18 @@ class CategoriaMailsScreens extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final categoriaMaels = LISTA_MEALS
+        .where(
+          (meal) => meal.categories!.contains(categoria.id),
+        )
+        .toList();
     return Scaffold(
       appBar: AppBar(
-        title: Text(categoria.id!),
+        title: Text(categoria.title!),
       ),
-      body: Center(
-        child: Text(categoria.title!),
+      body: ListView.builder(
+        itemCount: categoriaMaels.length,
+        itemBuilder: (ctx, index) => MealItem(categoriaMaels[index]),
       ),
     );
   }
