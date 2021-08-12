15a788ba401c2ca4d68f384b09f62633b4f7e1e0
Anfacibry <anfacibry@gmail.com>
2021-08-12 15:01:00 -0300

    atualizando models

--- lib/models/mealItem.dart
+++ lib/models/mealItem.dart
@@ -1,16 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:navegacao/models/meal.dart';
+import 'package:navegacao/utils/rotas_nomeadas.dart';
 
 class MealItem extends StatelessWidget {
   final Meal meal;
   const MealItem(this.meal, {Key? key}) : super(key: key);
 
-  void _selectMeal() {}
+  void _selectMeal(BuildContext context) {
+    Navigator.of(context).pushNamed(
+      AppRotas.MEAL_DETAIL,
+      arguments: meal,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: _selectMeal,
+      onTap: () => _selectMeal(context),
       child: Card(
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(15),
