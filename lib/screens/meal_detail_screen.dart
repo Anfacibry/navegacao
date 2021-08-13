c5e6857dd3cc072d38d1e250a6daeeee5fab6df7
Anfacibry <anfacibry@gmail.com>
2021-08-13 14:33:07 -0300

    atualizando lib/screens/meal_detail_screen.dart

--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -4,6 +4,30 @@ import 'package:navegacao/models/meal.dart';
 class MealDetailScreen extends StatelessWidget {
   const MealDetailScreen({Key? key}) : super(key: key);
 
+  Widget _createSectionTitle(BuildContext context, String titulo) {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 10),
+      child: Text(
+        titulo,
+        style: Theme.of(context).textTheme.bodyText2,
+      ),
+    );
+  }
+
+  Widget _createSectionContainer(Widget widget) {
+    return Container(
+      height: 200,
+      width: 350,
+      padding: EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border.all(color: Colors.grey),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: widget,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     final meal = ModalRoute.of(context)!.settings.arguments as Meal;
@@ -13,47 +37,56 @@ class MealDetailScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(meal.title!),
       ),
-      body: Column(
-        children: [
-          Container(
-            height: 250,
-            width: double.infinity,
-            child: Image.network(
-              meal.imageUrl!,
-              fit: BoxFit.cover,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 250,
+              width: double.infinity,
+              child: Image.network(
+                meal.imageUrl!,
+                fit: BoxFit.cover,
+              ),
             ),
-          ),
-          Container(
-            padding: EdgeInsets.symmetric(vertical: 10),
-            child: Text(
-              "Ingredientes",
-              style: Theme.of(context).textTheme.bodyText2,
+            _createSectionTitle(context, "Ingredientes"),
+            _createSectionContainer(
+              ListView.builder(
+                itemCount: meal.ingredients!.length,
+                itemBuilder: (cxt, index) {
+                  return Card(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 2, horizontal: 10),
+                      child: Text(meal.ingredients![index]),
+                    ),
+                    color: Theme.of(context).accentColor,
+                  );
+                },
+              ),
             ),
-          ),
-          Container(
-            height: 200,
-            width: 350,
-            padding: EdgeInsets.all(10),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              border: Border.all(color: Colors.grey),
-              borderRadius: BorderRadius.circular(10),
+            _createSectionTitle(context, "Passos"),
+            _createSectionContainer(
+              ListView.builder(
+                itemCount: meal.steps!.length,
+                itemBuilder: (ctx, index) {
+                  return Column(
+                    children: [
+                      ListTile(
+                        leading: CircleAvatar(
+                          child: Text("${index + 1}"),
+                        ),
+                        title: Text(meal.steps![index]),
+                      ),
+                      Divider(
+                        thickness: 2,
+                      ),
+                    ],
+                  );
+                },
+              ),
             ),
-            child: ListView.builder(
-              itemCount: meal.ingredients!.length,
-              itemBuilder: (cxt, index) {
-                return Card(
-                  child: Padding(
-                    padding:
-                        const EdgeInsets.symmetric(vertical: 2, horizontal: 10),
-                    child: Text(meal.ingredients![index]),
-                  ),
-                  color: Theme.of(context).accentColor,
-                );
-              },
-            ),
-          )
-        ],
+          ],
+        ),
       ),
     );
   }
