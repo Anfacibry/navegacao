76d8554024f9542ed68a3d872e44a150376457ee
Anfacibry <anfacibry@gmail.com>
2021-08-26 16:24:54 -0300

    atualizando

--- lib/screens/categoriasMailsScreens.dart
+++ lib/screens/categoriasMailsScreens.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:navegacao/data/dummy_data.dart';
 import 'package:navegacao/models/categoria.dart';
+import 'package:navegacao/models/meal.dart';
 import 'package:navegacao/models/mealItem.dart';
 
 class CategoriaMailsScreens extends StatelessWidget {
+  final List<Meal> meals;
   final Categoria categoria;
-  const CategoriaMailsScreens(this.categoria, {Key? key}) : super(key: key);
+  const CategoriaMailsScreens(this.categoria, this.meals, {Key? key})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final categoriaMaels = LISTA_MEALS
+    final categoriaMaels = meals
         .where(
           (meal) => meal.categories!.contains(categoria.id),
         )
